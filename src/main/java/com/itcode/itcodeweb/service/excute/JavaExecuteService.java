b477fa4800d153c6e9ea49e3db2da46f5505372e
thanh dat <dattran01477@gmail.com>
2020-08-05 23:03:42 +0700

    update compile for java

--- src/main/java/com/itcode/itcodeweb/service/excute/JavaExecuteService.java
+++ src/main/java/com/itcode/itcodeweb/service/excute/JavaExecuteService.java
@@ -1,9 +1,19 @@
 package com.itcode.itcodeweb.service.excute;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import com.itcode.itcodeweb.model.app.CodeAndTestCaseSubmit;
+import com.itcode.itcodeweb.model.app.TestCase;
 import com.itcode.itcodeweb.model.domain.CodeTemplate;
 import com.itcode.itcodeweb.model.respone.CodeResult;
 
@@ -27,7 +37,33 @@ public class JavaExecuteService extends AbstractExecuteCodeAndTestService {
 
 	@Override
 	protected CodeResult processCodeResult(CodeResult codeResult, CodeAndTestCaseSubmit codeSubmit) {
+		// Process output
+		Map<String, TestCase> mapResult = new HashMap<>();
+
+		String regex = "(?<=\\{\\{)(.*?[^?])(?=\\}\\})";
+		StringBuilder testCaseResult = new StringBuilder(
+				codeResult.getSuccessMessage().getSuccessComplieMessage().toString());
+		Pattern patternAllTestCase = Pattern.compile(regex);
+		Matcher matcherAllTestCase = patternAllTestCase.matcher(testCaseResult);
+		while (matcherAllTestCase.find()) {
+			mapResult.put(matcherAllTestCase.group(1), new TestCase("", matcherAllTestCase.group(1), false));
+		}
+
+		String regexCheckPassed = "(?<=\\{\\{\\?)(.*?)(?=\\?\\}\\})";
+		StringBuilder testCaseResultPassed = new StringBuilder(
+				codeResult.getSuccessMessage().getSuccessComplieMessage().toString());
+		Pattern patternCheckTestCasePassed = Pattern.compile(regexCheckPassed);
+		Matcher matcherCheckTestCasePassed = patternCheckTestCasePassed.matcher(testCaseResultPassed);
+		while (matcherCheckTestCasePassed.find()) {
+			mapResult.put(matcherCheckTestCasePassed.group(1), new TestCase("", matcherCheckTestCasePassed.group(1), true));
+		}
+
+		for (Map.Entry<String, TestCase> entry : mapResult.entrySet()) {
+			codeResult.getTestCasesResult().add(entry.getValue());
+		}
+
 		return codeResult;
+
 	}
 
 	@Override
